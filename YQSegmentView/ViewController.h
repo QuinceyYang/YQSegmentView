fe5f7eb4e8365f20618d1062522ac75ef8b2dce9
QuinceyYang <yqing555@qq.com>
2018-09-26 18:59:28 +0800

    Initial Commit

---
+++ YQSegmentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQSegmentView
+//
+//  Created by 杨清 on 2018/9/26.
+//  Copyright © 2018年 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
